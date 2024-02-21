e1430ad2fe826572dd0227013bb2b36b515b6d4d
checkmaathy <67769338+CheckMaathy@users.noreply.github.com>
2024-02-20 23:42:15 -0300

    first commit

---
+++ src/main/java/com/github/checkmaathy/SweetGyApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.checkmaathy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweetGyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SweetGyApiApplication.class, args);
+    }
+
+}
